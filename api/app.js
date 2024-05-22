444fe8c77810f2aa3979ce6c1c02a07e6dd2b6e3
Vinh-eta <vinhln@ethan-tech.com>
2024-05-22 17:02:42 +0700

    config prisma

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import postRoute from './routes/post.route.js';
+import authRoute from './routes/auth.route.js';
 const app = express();
 const PORT = 8800;
 
-app.use("/api/test", (req, res) => {
-  res.send("It works");
-});
+app.use(express.json());
+
+app.use("/api/posts", postRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(PORT, () => {
   console.log("Server is running");
