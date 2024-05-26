b2f7ec0125d0db466390028df6e7d43cf3edb1e0
Vinh Le <lengocvinh.729@gmail.com>
2024-05-26 20:52:24 +0700

    apply api register

--- client/estate-app/src/routes/register/register.jsx
+++ client/estate-app/src/routes/register/register.jsx
@@ -1,16 +1,62 @@
 import "./register.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      console.log(res.data);
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+
+    console.log(username, email, password);
+  };
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="email" type="text" placeholder="Email" />
-          <input name="password" type="password" placeholder="Password" />
+          <input
+            name="username"
+            type="text"
+            placeholder="Username"
+            title="username"
+            required
+          />
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            title="email"
+            required
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            title="password"
+            required
+          />
           <button>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
