9dd5aa77aa18ec767e11bd1f3bb1a76b41b3fbac
Vinh Le <lengocvinh.729@gmail.com>
2024-07-19 15:02:52 +0700

    update margin layout

--- client/estate-app/src/App.tsx
+++ client/estate-app/src/App.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <div>
-      <ul className="text-blue-400 p-4 list-disc">
+      <ul className="text-blue-400 p-4 list-disc ml-6">
         <li className="hover:text-blue-800">
           <Link to="/todo-list">Todo List</Link>
         </li>
