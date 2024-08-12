c912bc2ff63cb61c21c32a11bf06fc9868230468
Vinh Le <lengocvinh.729@gmail.com>
2024-08-12 10:33:21 +0700

    remove dist cache

--- api/project-nestjs/dist/book/book.providers.d.ts
+++
@@ -1,7 +0,0 @@
-import { DataSource } from 'typeorm';
-import { Book } from '../typeorm/book.entity';
-export declare const photoProviders: {
-    provide: string;
-    useFactory: (dataSource: DataSource) => import("typeorm").Repository<Book>;
-    inject: string[];
-}[];
--- api/project-nestjs/dist/typeorm/book.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractEntity } from './Abstract.entity';
-export declare class Book extends AbstractEntity {
-    id: number;
-    author: string;
-    name: string;
-    rating: string;
-    status: string;
-}
